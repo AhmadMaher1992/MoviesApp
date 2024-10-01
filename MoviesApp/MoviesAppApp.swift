a7e646ff4cf45851e56f67099e7c373b182dec19
AhmadMaher1992 <ahmadeisa1992@gmail.com>
2024-10-01 09:23:55 +0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Ahmad Eisa on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesAppTests/MoviesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppTests.swift
+//  MoviesAppTests
+//
+//  Created by Ahmad Eisa on 01/10/2024.
+//
+
+import Testing
+@testable import MoviesApp
+
+struct MoviesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
