378be6772d8dff0ba905837baa19bbf504901257
AhmadMaher1992 <ahmadeisa1992@gmail.com>
2024-10-01 10:04:00 +0300

    generate my api key from themoviedb site

---
+++ MoviesApp/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  MoviesApp
+//
+//  Created by Ahmad Eisa on 01/10/2024.
+//
+
+let API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZTg4MTNhMzc0MGNhYWVjM2E1MmE4OWM4NmNkMTI2ZiIsIm5iZiI6MTcyNzc2NTc1MS40MDI4MSwic3ViIjoiNWU4YjFhMTk5OTc5ZDIwMDE5ZWU3N2I2Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.7VgTI0uHjqUGq87QRhHm-1WCCoQkV3EU-c0pddyOV2Y"
